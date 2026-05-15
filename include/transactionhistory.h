2974fe2ae1a4263ab6a8796e478b33c51d3d68d8
Alkatraz16 <golian302@gmail.com>
2026-05-15 23:59:45 +0800

    feat: add transaction history header file with function declarations

---
+++ include/transactionhistory.h
@@ -0,0 +1,12 @@
+#ifndef TRANSACTIONHISTORY_H
+#define TRANSACTIONHISTORY_H
+
+#include "transactions.h"
+#include "patients.h"
+
+void transactionHistoryMenu();
+void viewAllTransactionHistory();
+void viewTransactionsByPatient();
+void searchTransactions();
+
+#endif
